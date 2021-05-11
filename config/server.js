22470c4718e35299bf6c29642e78869fce02e0fe
Bryan <ck20200420@gmail.com>
2021-05-11 14:01:07 +0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '40c1cf1eb929f9237feb678f72b49bd4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '727b49d9-13e2-438f-b7d4-075b57464b56'
+};
