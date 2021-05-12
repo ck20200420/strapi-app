f95ddf45ad8b69e051c0c7b8372536b1803fdd26
Bryan <ck20200420@gmail.com>
2021-05-12 10:43:59 +0800

    subdomain

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://api.ibet1788.com',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '40c1cf1eb929f9237feb678f72b49bd4'),
